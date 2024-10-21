c0df2a01590256e387e1cdc9937bbd5facad2de5
Matias Hernandez <matiashernandezdonbosco@gmail.com>
2024-10-21 12:44:07 -0300

    Comentarios

--- test/LibraryTests/Items/SpellTest.cs
+++ test/LibraryTests/Items/SpellTest.cs
@@ -8,16 +8,19 @@ namespace LibraryTests.Items;
 [TestSubject(typeof(Spell))]
 public class SpellTEST
 {
-
+    //Con este metodo se verifica la correcta creacion de un hechizo
     [TestMethod]
     public void CrearSpell()
-    {
+    {   //Se define el nombre del hechizo
         string nombre = "Bola de Fuego";
+        //Se define como entero su valor de ataque
         int valorAtaque = 50;
+        //Se define como entero el valor de defensa que tiene
         int valorDefensa = 20;
-
-
+        
+        //Se crea una nueva instancia de Spell con el nombre definido, Valor de ataque y valor de defensa
         Spell boladefuego = new Spell(nombre, valorAtaque,valorDefensa);
+        //Se verifica que el nombre del hechizo sea igual al previsto
         Assert.AreEqual(nombre, boladefuego.Name); 
         Assert.AreEqual(valorAtaque, boladefuego.AttackValue); 
         Assert.AreEqual(valorDefensa, boladefuego.DefenseValue); 
--- test/LibraryTests/Items/SpellsBookTest.cs
+++ test/LibraryTests/Items/SpellsBookTest.cs
@@ -11,43 +11,53 @@ public class SpellsBookTEST
 
     [TestMethod]
     public void CrearLibro()
-    {
+    {   //Se crea variable Grimorio
         string nombre = "Grimorio";
+        //Se crea una lista que incluye dos hechizos 
         List<Spell> hechizos = new List<Spell>
         {
             new Spell("Bola de fuego", 50, 20),
             new Spell("Escudo", 0, 30)
         };
-
+        //Se crea el grimorio usando el nombre y la lista de hechizos
         SpellsBook grimorio = new SpellsBook(nombre, hechizos);
-
+        //Se verifica que el nombre del grimorio sea el esperado: "Grimorio"
         Assert.AreEqual(nombre, grimorio.Name);
+        //Se verifica que los hechizos del grimorio coincidan con la lista proporcionada"
         Assert.AreEqual(hechizos, grimorio.Spells);
     }
 
     [TestMethod]
     public void AgregaryRemoverHechizo()
-    {
+    {   //Se inicializa un grimorio
         SpellsBook grimorio = new SpellsBook("Grimorio", new List<Spell>());
+        //Se inicializa un nuevo hechizo
         Spell boladefuego = new Spell("Bola de fuego", 50, 20);
+        //Se añade el hechizo al grimorio
         grimorio.AddSpell(boladefuego);
+        //Se verifica que el grimorio contenga el hechizo previsto
         CollectionAssert.Contains(grimorio.Spells,boladefuego);
+        //Se remueve el hechizo del grimorio
         grimorio.RemoveSpell(boladefuego);
+        //Se verifica que el grimorio ya no contiene el hechizo eliminado previamente
         Assert.IsFalse(grimorio.Spells.Contains(boladefuego));
     }
 
     [TestMethod]
     public void ValorAtaqueDefensa()
-    {
+    {   //Se crea un hechizo con nombre, valor de ataque y valor de defensa
         Spell boladefuego = new Spell("Bola de fuego", 50, 20);
+        //Se crea un escudo unicamente con valor de defensa
         Spell escudo = new Spell("Escudo", 0, 30);
-        
+        //Se crea un grimorio con el hechizo y el escudo
         SpellsBook grimorio = new SpellsBook("Grimorio", new List<Spell>{boladefuego,escudo});
-
+        //Se setea un ataque esperado por parte del hechizo
         int AtaqueEsperado = 50;
+        //Se verifica que el valor de ataque calculado por el grimorio coincida con el valor esperado
         Assert.AreEqual(AtaqueEsperado, grimorio.AttackValueWithSpell());
-
-        int DefensaEsperada = 50;
+        //Esta vez se setea un valor de defensa esperado por parte del escudo
+        int DefensaEsperada = 20;
+        //Se verifica que el valor de la defensa del grimorio coincida con la defensa esperada
         Assert.AreEqual(DefensaEsperada, grimorio.AttackValueWithSpell());
 
     }
