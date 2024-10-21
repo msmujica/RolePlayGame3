e1fbad64d235626e9b8c606243cfd71a793ddd03
Matias Hernandez <matiashernandezdonbosco@gmail.com>
2024-10-21 10:02:39 -0300

    Se arreglan los Test de Spell y SpellBook, funcionan perfectamente

--- test/LibraryTests/Items/SpellTest.cs
+++ test/LibraryTests/Items/SpellTest.cs
@@ -1,14 +1,15 @@
-using NUnit.Framework;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ucu.Poo.RoleplayGame.Program.Items;
 
 namespace LibraryTests.Items;
 
-[TestFixture]
-[TestOf(typeof(Spell))]
+[TestClass]
+[TestSubject(typeof(Spell))]
 public class SpellTEST
 {
 
-    [Test]
+    [TestMethod]
     public void CrearSpell()
     {
         string nombre = "Bola de Fuego";
@@ -17,8 +18,8 @@ public class SpellTEST
 
 
         Spell boladefuego = new Spell(nombre, valorAtaque,valorDefensa);
-        //Assert.AreEqual(nombre, boladefuego.Name); 
-        //Assert.AreEqual(valorAtaque, boladefuego.AttackValue); 
-        //Assert.AreEqual(valorDefensa, boladefuego.DefenseValue); 
+        Assert.AreEqual(nombre, boladefuego.Name); 
+        Assert.AreEqual(valorAtaque, boladefuego.AttackValue); 
+        Assert.AreEqual(valorDefensa, boladefuego.DefenseValue); 
     }
 }
